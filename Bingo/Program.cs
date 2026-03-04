cafe79f09daeb47c2b38c6616d0d47601d95116e
Petrik Diak <diak@petrik.hu>
2026-03-04 08:59:34 +0100

    3. Feladat: Listaban tárolás. ha egyet tud beolvasni akkor Andi

--- Bingo/Program.cs
+++ Bingo/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
-        }
+			List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+
+			if (File.Exists("nevek.text"))
+			{
+				foreach (var fajl in File.ReadAllLines("nevek.text"))
+				{
+					if (File.Exists(fajl))
+					{
+						jatekosok.Add(new BingoJatekos(fajl));
+					}
+				}
+			}
+			else if (File.Exists("Andi.txt"))
+			{
+				jatekosok.Add(new BingoJatekos("Andi.txt"));
+			}
+		}
     }
 }
